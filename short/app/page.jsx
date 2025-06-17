8e3e7a2178a393fb8f12259620b5bb45d1d90fff
editid0 <66117959+editid0@users.noreply.github.com>
2025-06-17 14:57:37 +0100

    feat: add URL validation and shorten button functionality

--- short/app/page.jsx
+++ short/app/page.jsx
@@ -1,13 +1,25 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Home() {
 	const [url, setUrl] = useState("");
+	const [isValid, setIsValid] = useState("");
 	const handleChange = (e) => {
 		setUrl(e.target.value);
 	};
+	useEffect(() => {
+		if (url === "") {
+			setIsValid("");
+			return;
+		}
+		const regex =
+			/^(https?:\/\/)?([\w-]+(\.[\w-]+)+)(:\d+)?(\/[\w-./?%&=]*)?$/;
+		setIsValid(regex.test(url));
+	}, [url]);
 	return (
 		<>
 			<h1 className="text-center text-4xl font-semibold mt-2">
@@ -18,7 +30,23 @@ export default function Home() {
 					value={url}
 					onChange={handleChange}
 					placeholder="Enter URL"
+					className={
+						isValid === false
+							? "border-red-500"
+							: isValid === true
+							? "border-green-500"
+							: ""
+					}
 				/>
+			</div>
+			<div className="flex justify-center mt-4">
+				<Button asChild>
+					<Link
+						href={`/shorten?url=${encodeURIComponent(btoa(url))}`}
+					>
+						Shorten
+					</Link>
+				</Button>
 			</div>
 		</>
 	);
