bdc07e9c6920388bd1251844079e146e0920fb34
editid0 <66117959+editid0@users.noreply.github.com>
2025-06-23 00:53:15 +0100

    refactor: improve formatting and structure of Data component and page logic

--- short/app/urls/data.jsx
+++ short/app/urls/data.jsx
@@ -2,230 +2,305 @@
 
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
-import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import {
+	DropdownMenu,
+	DropdownMenuCheckboxItem,
+	DropdownMenuContent,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, getSortedRowModel, useReactTable } from "@tanstack/react-table";
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
+import {
+	flexRender,
+	getCoreRowModel,
+	getFilteredRowModel,
+	getPaginationRowModel,
+	getSortedRowModel,
+	useReactTable,
+} from "@tanstack/react-table";
+import { ExternalLink } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
 
 function stripProtocol(url) {
-    return url.replace(/(^\w+:|^)\/\//, '');
+	return url.replace(/(^\w+:|^)\/\//, "");
+}
+
+function truncateString(str, maxLength = 20) {
+	if (str.length <= maxLength) return str;
+	return str.slice(0, maxLength) + "...";
 }
 
 export default function Data({ data }) {
-    console.log("Data received:", data);
-    const columns = [
-        {
-            id: "select",
-            header: ({ table }) => (
-                <Checkbox
-                    checked={
-                        table.getIsAllPageRowsSelected() ||
-                        (table.getIsSomePageRowsSelected() && "indeterminate")
-                    }
-                    onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-                    aria-label="Select all"
-                />
-            ),
-            cell: ({ row }) => (
-                <Checkbox
-                    checked={row.getIsSelected()}
-                    onCheckedChange={(value) => row.toggleSelected(!!value)}
-                    aria-label="Select row"
-                />
-            ),
-            enableSorting: false,
-            enableHiding: false,
-        },
-        {
-            id: "url",
-            header: "URL",
-            cell: ({ row }) => (
-                // Strip http:// or https:// from the URL
-                <Link href={row.original.url} target="_blank" rel="noopener noreferrer">
-                    {stripProtocol(row.original.url)}
-                </Link>
-            ),
-            enableSorting: true,
-            enableHiding: true,
-        },
-        {
-            id: "slug",
-            header: "Slug",
-            cell: ({ row }) => (
-                <Link href={`/s/${row.original.slug}`} target="_blank" rel="noopener noreferrer">
-                    {row.original.slug}
-                </Link>
-            ),
-            enableSorting: true,
-            enableHiding: true,
-        },
-        {
-            id: "expiry_date",
-            header: "Expiry Date",
-            cell: ({ row }) => row.original.expiry_date ? new Date(row.original.expiry_date).toLocaleDateString() : "Never",
-            enableSorting: true,
-            enableHiding: true,
-        },
-        {
-            id: "expiration_time",
-            header: "Expiration Time",
-            cell: ({ row }) => row.original.expiration_time ? new Date(`1970-01-01T${row.original.expiration_time}`).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) : "No Time",
-            enableSorting: true,
-            enableHiding: true,
-        },
-        {
-            id: "created_at",
-            header: "Created At",
-            cell: ({ row }) => new Date(row.original.created_at).toLocaleString(),
-            enableSorting: true,
-            enableHiding: true,
-        },
-        {
-            id: "updated_at",
-            header: "Updated At",
-            cell: ({ row }) => new Date(row.original.updated_at).toLocaleString(),
-            enableSorting: true,
-            enableHiding: true,
-        }
-    ];
+	console.log("Data received:", data);
+	const columns = [
+		{
+			id: "select",
+			header: ({ table }) => (
+				<Checkbox
+					checked={
+						table.getIsAllPageRowsSelected() ||
+						(table.getIsSomePageRowsSelected() && "indeterminate")
+					}
+					onCheckedChange={(value) =>
+						table.toggleAllPageRowsSelected(!!value)
+					}
+					aria-label="Select all"
+				/>
+			),
+			cell: ({ row }) => (
+				<Checkbox
+					checked={row.getIsSelected()}
+					onCheckedChange={(value) => row.toggleSelected(!!value)}
+					aria-label="Select row"
+				/>
+			),
+			enableSorting: false,
+			enableHiding: false,
+		},
+		{
+			id: "url",
+			header: "URL",
+			cell: ({ row }) => (
+				// Strip http:// or https:// from the URL
+				<Link
+					href={row.original.url}
+					target="_blank"
+					rel="noopener noreferrer"
+					className="dark:text-blue-400 text-blue-500 hover:underline"
+				>
+					{truncateString(stripProtocol(row.original.url), 15)}
+				</Link>
+			),
+			enableSorting: true,
+			enableHiding: true,
+		},
+		{
+			id: "slug",
+			header: "Slug",
+			cell: ({ row }) => (
+				<Link
+					href={`/s/${row.original.slug}`}
+					target="_blank"
+					rel="noopener noreferrer"
+					className="dark:text-blue-400 text-blue-500 hover:underline"
+				>
+					{truncateString(row.original.slug, 15)}
+				</Link>
+			),
+			enableSorting: true,
+			enableHiding: true,
+		},
+		{
+			id: "expiry_date",
+			header: "Expiry Date",
+			cell: ({ row }) =>
+				row.original.expiry_date
+					? new Date(row.original.expiry_date).toLocaleDateString()
+					: "Never",
+			enableSorting: true,
+			enableHiding: true,
+		},
+		{
+			id: "expiration_time",
+			header: "Expiration Time",
+			cell: ({ row }) =>
+				row.original.expiration_time
+					? new Date(
+							`1970-01-01T${row.original.expiration_time}`
+					  ).toLocaleTimeString([], {
+							hour: "2-digit",
+							minute: "2-digit",
+					  })
+					: "No Time",
+			enableSorting: true,
+			enableHiding: true,
+		},
+		{
+			id: "created_at",
+			header: "Created At",
+			cell: ({ row }) =>
+				new Date(row.original.created_at).toLocaleString(),
+			enableSorting: true,
+			enableHiding: true,
+		},
+		{
+			id: "updated_at",
+			header: "Updated At",
+			cell: ({ row }) =>
+				new Date(row.original.updated_at).toLocaleString(),
+			enableSorting: true,
+			enableHiding: true,
+		},
+		{
+			id: "actions",
+			header: "Edit",
+			cell: ({ row }) => (
+				<Button variant="outline" asChild>
+					<Link
+						href={`/urls/edit/${row.original.id}`}
+						className="text-blue-500 hover:underline"
+					>
+						Edit
+					</Link>
+				</Button>
+			),
+			enableSorting: false,
+			enableHiding: true,
+		},
+	];
 
-    const [sorting, setSorting] = useState([]);
-    const [columnFilters, setColumnFilters] = useState([]);
-    const [columnVisibility, setColumnVisibility] = useState({});
-    const [rowSelection, setRowSelection] = useState({});
-    const table = useReactTable({
-        data,
-        columns,
-        onSortingChange: setSorting,
-        onColumnFiltersChange: setColumnFilters,
-        getCoreRowModel: getCoreRowModel(),
-        getPaginationRowModel: getPaginationRowModel(),
-        getSortedRowModel: getSortedRowModel(),
-        getFilteredRowModel: getFilteredRowModel(),
-        onColumnVisibilityChange: setColumnVisibility,
-        onRowSelectionChange: setRowSelection,
-        state: {
-            sorting,
-            columnFilters,
-            columnVisibility,
-            rowSelection,
-        },
-    });
+	const [sorting, setSorting] = useState([]);
+	const [columnFilters, setColumnFilters] = useState([]);
+	const [columnVisibility, setColumnVisibility] = useState({});
+	const [rowSelection, setRowSelection] = useState({});
+	const table = useReactTable({
+		data,
+		columns,
+		onSortingChange: setSorting,
+		onColumnFiltersChange: setColumnFilters,
+		getCoreRowModel: getCoreRowModel(),
+		getPaginationRowModel: getPaginationRowModel(),
+		getSortedRowModel: getSortedRowModel(),
+		getFilteredRowModel: getFilteredRowModel(),
+		onColumnVisibilityChange: setColumnVisibility,
+		onRowSelectionChange: setRowSelection,
+		state: {
+			sorting,
+			columnFilters,
+			columnVisibility,
+			rowSelection,
+		},
+	});
 
-    return (
-        <>
-            <div className="w-full">
-                <div className="flex items-center py-4">
-                    <Input
-                        placeholder="Filter URLs..."
-                        value={(table.getColumn("url")?.getFilterValue() ?? "")}
-                        onChange={(e) => table.getColumn("url")?.setFilterValue(e.target.value)}
-                        className="w-full max-w-sm"
-                    />
-                    <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <Button variant={"outline"} className="ml-auto">
-                                Actions
-                            </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent align="end">
-                            {table
-                                .getAllColumns()
-                                .filter((column) => column.getCanHide())
-                                .map((column) => {
-                                    return (
-                                        <DropdownMenuCheckboxItem
-                                            key={column.id}
-                                            className="capitalize"
-                                            checked={column.getIsVisible()}
-                                            onCheckedChange={(value) =>
-                                                column.toggleVisibility(!!value)
-                                            }
-                                        >
-                                            {column.id}
-                                        </DropdownMenuCheckboxItem>
-                                    )
-                                })}
-                        </DropdownMenuContent>
-                    </DropdownMenu>
-                </div>
-                <div className="rounded-md border">
-                    <Table>
-                        <TableHeader>
-                            {table.getHeaderGroups().map((headerGroup) => (
-                                <TableRow key={headerGroup.id}>
-                                    {headerGroup.headers.map((header) => {
-                                        return (
-                                            <TableHead key={header.id}>
-                                                {header.isPlaceholder
-                                                    ? null
-                                                    : flexRender(
-                                                        header.column.columnDef.header,
-                                                        header.getContext()
-                                                    )}
-                                            </TableHead>
-                                        )
-                                    })}
-                                </TableRow>
-                            ))}
-                        </TableHeader>
-                        <TableBody>
-                            {table.getRowModel().rows?.length ? (
-                                table.getRowModel().rows.map((row) => (
-                                    <TableRow
-                                        key={row.id}
-                                        data-state={row.getIsSelected() && "selected"}
-                                    >
-                                        {row.getVisibleCells().map((cell) => (
-                                            <TableCell key={cell.id}>
-                                                {flexRender(
-                                                    cell.column.columnDef.cell,
-                                                    cell.getContext()
-                                                )}
-                                            </TableCell>
-                                        ))}
-                                    </TableRow>
-                                ))
-                            ) : (
-                                <TableRow>
-                                    <TableCell
-                                        colSpan={columns.length}
-                                        className="h-24 text-center"
-                                    >
-                                        No results.
-                                    </TableCell>
-                                </TableRow>
-                            )}
-                        </TableBody>
-                    </Table>
-                </div>
-                <div className="flex items-center justify-end space-x-2 py-4">
-                    <div className="text-muted-foreground flex-1 text-sm">
-                        {table.getFilteredSelectedRowModel().rows.length} of{" "}
-                        {table.getFilteredRowModel().rows.length} row(s) selected.
-                    </div>
-                    <div className="space-x-2">
-                        <Button
-                            variant="outline"
-                            size="sm"
-                            onClick={() => table.previousPage()}
-                            disabled={!table.getCanPreviousPage()}
-                        >
-                            Previous
-                        </Button>
-                        <Button
-                            variant="outline"
-                            size="sm"
-                            onClick={() => table.nextPage()}
-                            disabled={!table.getCanNextPage()}
-                        >
-                            Next
-                        </Button>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+	return (
+		<>
+			<div className="w-full">
+				<div className="flex items-center py-4">
+					<Input
+						placeholder="Filter URLs..."
+						value={table.getColumn("url")?.getFilterValue() ?? ""}
+						onChange={(e) =>
+							table
+								.getColumn("url")
+								?.setFilterValue(e.target.value)
+						}
+						className="w-full max-w-sm"
+					/>
+					<DropdownMenu>
+						<DropdownMenuTrigger asChild>
+							<Button variant={"outline"} className="ml-auto">
+								Actions
+							</Button>
+						</DropdownMenuTrigger>
+						<DropdownMenuContent align="end">
+							{table
+								.getAllColumns()
+								.filter((column) => column.getCanHide())
+								.map((column) => {
+									return (
+										<DropdownMenuCheckboxItem
+											key={column.id}
+											className="capitalize"
+											checked={column.getIsVisible()}
+											onCheckedChange={(value) =>
+												column.toggleVisibility(!!value)
+											}
+										>
+											{column.id}
+										</DropdownMenuCheckboxItem>
+									);
+								})}
+						</DropdownMenuContent>
+					</DropdownMenu>
+				</div>
+				<div className="rounded-md border">
+					<Table>
+						<TableHeader>
+							{table.getHeaderGroups().map((headerGroup) => (
+								<TableRow key={headerGroup.id}>
+									{headerGroup.headers.map((header) => {
+										return (
+											<TableHead key={header.id}>
+												{header.isPlaceholder
+													? null
+													: flexRender(
+															header.column
+																.columnDef
+																.header,
+															header.getContext()
+													  )}
+											</TableHead>
+										);
+									})}
+								</TableRow>
+							))}
+						</TableHeader>
+						<TableBody>
+							{table.getRowModel().rows?.length ? (
+								table.getRowModel().rows.map((row) => (
+									<TableRow
+										key={row.id}
+										data-state={
+											row.getIsSelected() && "selected"
+										}
+									>
+										{row.getVisibleCells().map((cell) => (
+											<TableCell key={cell.id}>
+												{flexRender(
+													cell.column.columnDef.cell,
+													cell.getContext()
+												)}
+											</TableCell>
+										))}
+									</TableRow>
+								))
+							) : (
+								<TableRow>
+									<TableCell
+										colSpan={columns.length}
+										className="h-24 text-center"
+									>
+										No results.
+									</TableCell>
+								</TableRow>
+							)}
+						</TableBody>
+					</Table>
+				</div>
+				<div className="flex items-center justify-end space-x-2 py-4">
+					<div className="text-muted-foreground flex-1 text-sm">
+						{table.getFilteredSelectedRowModel().rows.length} of{" "}
+						{table.getFilteredRowModel().rows.length} row(s)
+						selected.
+					</div>
+					<div className="space-x-2">
+						<Button
+							variant="outline"
+							size="sm"
+							onClick={() => table.previousPage()}
+							disabled={!table.getCanPreviousPage()}
+						>
+							Previous
+						</Button>
+						<Button
+							variant="outline"
+							size="sm"
+							onClick={() => table.nextPage()}
+							disabled={!table.getCanNextPage()}
+						>
+							Next
+						</Button>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
